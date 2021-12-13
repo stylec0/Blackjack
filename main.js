63108c831a075776d2af7240ddb098b2e25f7ebc
Charles Co <stylec0-mac@Charless-MacBook-Air.local>
2021-12-13 15:42:10 -0800

    Working on generating shuffled deck. 3:42PM, 12/13/21

--- main.js
+++ main.js
@@ -6,70 +6,26 @@ const ranks = ['02', '03', '04', '05', '06', '07', '08', '09', '10', 'J', 'Q', '
 
 // Build a 'master' deck of 'card' objects used to create shuffled decks
 const masterDeck = buildMasterDeck();
-renderDeckInContainer(masterDeck, document.getElementById('master-deck-container'));
-
-// /*----- cached element references -----*/
-const shuffledContainer = document.getElementById('shuffled-deck-container');
-const dealBtn = document.getElementById('start-game');
-const standBtn = document.getElementById('stand');
-const hitBtn = document.getElementById('hit');
 
 /*----- app's state (variables) -----*/
 let shuffledDeck;
-
-// cached element references //
 let dealerHand = [];
 let playerHand = [];
-let dealerScore = 0;
-let playerScore = 0;
+
+// /*----- cached element references -----*/
+const startGameBtn = document.getElementById('start');
+const standBtn = document.getElementById('stand');
+const hitBtn = document.getElementById('hit');
 
 /*----- event listeners -----*/
-startGameButton.addEventListener('click', startGame);
+startGameBtn.addEventListener('click', start);
+hitBtn.addEventListener('click', hit);
+standBtn.addEventListener('click', stand);
 
 /*----- functions -----*/
 
-// Create function to deal player cards into player hand
-function dealPlayerCards () {
-    for (let i; i < 2; i++){
-        let card = shuffledDeck.pop()
-        playerHand.push(card); 
-    }
-}
 
-function getNewShuffledDeck() {
-    // Create a copy of the masterDeck (leave masterDeck untouched!)
-    const tempDeck = [...masterDeck];
-    const newShuffledDeck = [];
-    while (tempDeck.length) {
-      // Get a random index for a card still in the tempDeck
-      const rndIdx = Math.floor(Math.random() * tempDeck.length);
-      // Note the [0] after splice - this is because splice always returns an array and we just want the card object in that array
-      newShuffledDeck.push(tempDeck.splice(rndIdx, 1)[0]);
-    }
-    return newShuffledDeck;
-  }
-  
-  function renderNewShuffledDeck() {
-    // Create a copy of the masterDeck (leave masterDeck untouched!)
-    shuffledDeck = getNewShuffledDeck();
-    renderDeckInContainer(shuffledDeck, shuffledContainer);
-  }
-  
-  function renderDeckInContainer(deck, container) {
-    container.innerHTML = '';
-    // Let's build the cards as a string of HTML
-    let cardsHtml = '';
-    deck.forEach(function(card) {
-      cardsHtml += `<div class="card ${card.face}"></div>`;
-    });
-    // Or, use reduce to 'reduce' the array into a single thing - in this case a string of HTML markup 
-    // const cardsHtml = deck.reduce(function(html, card) {
-    //   return html + `<div class="card ${card.face}"></div>`;
-    // }, '');
-    container.innerHTML = cardsHtml;
-  }
-  
-  function buildMasterDeck() {
+function buildMasterDeck() {
     const deck = [];
     // Use nested forEach to generate card objects
     suits.forEach(function(suit) {
@@ -84,42 +40,137 @@ function getNewShuffledDeck() {
     });
     return deck;
   }
-  
-  renderNewShuffledDeck();
 
+function shuffleDeck() {
+    // Create a copy of the masterDeck (leave masterDeck untouched!)
+    const tempDeck = [...masterDeck];
+    const shuffledDeck = [];
+    while (tempDeck.length) {
+      // Get a random index for a card still in the tempDeck
+      const rndIdx = Math.floor(Math.random() * tempDeck.length);
+      // Note the [0] after splice - this is because splice always returns an array and we just want the card object in that array
+      shuffledDeck.push(tempDeck.splice(rndIdx, 1)[0]);
+    }
+    return shuffledDeck;
+  }
+  
   // We call init, because we want to initialize our state when the page loads
 
   function init() {
-    dealPlayerCards();
-    getNewShuffledDeck() ;
-    renderNewShuffledDeck() ;
     buildMasterDeck();
+    getNewShuffledDeck();
+    shuffledDeck(masterDeck);
+    
   }
 
-  init();
 
 
 
 
-// const handContainer = {
-//     player: document.querySelector('playerHand'),
-//     dealer: document.querySelector('dealerHand'),
+  // Create function to deal player cards into player hand
+// function dealPlayerCards () {
+//     for (let i; i < 2; i++){
+//         let card = shuffledDeck.pop()
+//         playerHand.push(card); 
+//     }
 // }
 
-// const dealBtn = document.getElementById('deal');
-// const standBtn = document.getElementById('stand');
-// const hitBtn = document.getElementById('hit');
-// const resultScreen = document.getElementById('')
 
+
+// // Build a 'master' deck of 'card' objects used to create shuffled decks
+// const masterDeck = buildMasterDeck();
+// renderDeckInContainer(masterDeck, document.getElementById('master-deck-container'));
+
+// // /*----- cached element references -----*/
+// // const shuffledContainer = document.getElementById('shuffled-deck-container');
+// // const startGameBtn = document.getElementById('start-game');
+// // const standBtn = document.getElementById('stand');
+// // const hitBtn = document.getElementById('hit');
+
+// /*----- app's state (variables) -----*/
+// let shuffledDeck;
+
+// // cached element references //
+// let dealerHand = [];
+// let playerHand = [];
+// // let dealerScore = 0;
+// // let playerScore = 0;
 
 // /*----- event listeners -----*/
-// document.querySelector('button').addEventListener('click', renderNewShuffledDeck);
-// startGameButton.addEventListener('click', startGame);
-// standButton.addEventListener('click', stand);
-// hitButton.addEventListener('click', hit);
+// startGameBtn.addEventListener('click', startGame);
+// hitBtn.addEventListener('click', hit);
+// standBtn.addEventListener('click', stand);
 
-// // /*----- functions -----*/
+// /*----- functions -----*/
 
+// // Create function to deal player cards into player hand
+// function dealPlayerCards () {
+//     for (let i; i < 2; i++){
+//         let card = shuffledDeck.pop()
+//         playerHand.push(card); 
+//     }
+// }
+
+// function getNewShuffledDeck() {
+//     // Create a copy of the masterDeck (leave masterDeck untouched!)
+//     const tempDeck = [...masterDeck];
+//     const newShuffledDeck = [];
+//     while (tempDeck.length) {
+//       // Get a random index for a card still in the tempDeck
+//       const rndIdx = Math.floor(Math.random() * tempDeck.length);
+//       // Note the [0] after splice - this is because splice always returns an array and we just want the card object in that array
+//       newShuffledDeck.push(tempDeck.splice(rndIdx, 1)[0]);
+//     }
+//     return newShuffledDeck;
+//   }
+  
+//   function renderNewShuffledDeck() {
+//     // Create a copy of the masterDeck (leave masterDeck untouched!)
+//     shuffledDeck = getNewShuffledDeck();
+//     renderDeckInContainer(shuffledDeck, shuffledContainer);
+//   }
+  
+//   function renderDeckInContainer(deck, container) {
+//     container.innerHTML = '';
+//     // Let's build the cards as a string of HTML
+//     let cardsHtml = '';
+//     deck.forEach(function(card) {
+//       cardsHtml += `<div class="card ${card.face}"></div>`;
+//     });
+//     // Or, use reduce to 'reduce' the array into a single thing - in this case a string of HTML markup 
+//     // const cardsHtml = deck.reduce(function(html, card) {
+//     //   return html + `<div class="card ${card.face}"></div>`;
+//     // }, '');
+//     container.innerHTML = cardsHtml;
+//   }
+  
+//   function buildMasterDeck() {
+//     const deck = [];
+//     // Use nested forEach to generate card objects
+//     suits.forEach(function(suit) {
+//       ranks.forEach(function(rank) {
+//         deck.push({
+//           // The 'face' property maps to the library's CSS classes for cards
+//           face: `${suit}${rank}`,
+//           // Setting the 'value' property for game of blackjack, not war
+//           value: Number(rank) || (rank === 'A' ? 11 : 10)
+//         });
+//       });
+//     });
+//     return deck;
+//   }
+  
+
+//   // We call init, because we want to initialize our state when the page loads
+
+//   function init() {
+//     dealPlayerCards();
+//     getNewShuffledDeck() ;
+//     renderNewShuffledDeck() ;
+//     buildMasterDeck();
+//   }
+
+//   init();
 
 
 
