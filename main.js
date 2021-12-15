da30ccccd1d60a2fe19c64c6a9410dc5650c0206
Charles Co <stylec0-mac@Charless-MacBook-Air.local>
2021-12-14 17:51:19 -0800

    Dealer function updates 5:51pm, 12/14/21

--- main.js
+++ main.js
@@ -47,16 +47,6 @@ function dealDealerCards () {
     showDealerHand();
 }
 
-// function showPlayerHand () { 
-//     // get playerHand data, 
-//     console.log(playerHand)
-//     // Display PlayerHand 
-//     console.log(playerDeckHand)
-//     playerDeckHand.innerHTML = `${playerHand[0].face} ${playerHand[1].face}`;
-//     newDeck();
-//     }
-    
-
 function showPlayerHand() {
     playerDeckHand.innerHTML = '';
     playerHand.forEach(function(card) {
@@ -83,6 +73,7 @@ function playerHit() {
         let cards = `<div class = "card ${card.face}"></div>`
         playerDeckHand.innerHTML += cards;
         playerCardValues();
+        gameLogic() 
   }
 
   function dealerHit() {
@@ -91,19 +82,14 @@ function playerHit() {
         let cards = `<div class = "card ${card.face}"></div>`
         dealerDeckHand.innerHTML += cards;
         dealerCardValues();
+        gameLogic() 
   }
 
 function dealerTurn() {
   if (dealerTotal > 17) {
-    console.log("dealer hits")
-    dealerHit();
-  }else if(dealerTotal >= 17) {
-    console.log("dealer stands")
-  }else if (dealerTotal === 21) {
-    console.log("Black Jack! Dealer Wins")
-    } else { (dealerTotal > 21) 
-      console.log("Dealer Busts! Player Wins!")
+      dealerHit();
     }
+    gameLogic()
   }
 
 
@@ -111,8 +97,8 @@ function dealerTurn() {
 
 function stand() {
   dealerTurn();
-  playerCardValues();
-  dealerCardValues()
+  playerCardValues()
+  dealerCardValues() 
 }
 
 
@@ -165,6 +151,7 @@ function shuffleDeck() {
     render();
     playerCardValues();
     dealerCardValues();
+    gameLogic()
   }
 
 // Game Logic
@@ -174,7 +161,6 @@ function playerCardValues() {
   for (let i = 0; i < playerHand.length; i++) {
     playerTotal += playerHand[i].value;
   }
-  gameLogic();
 }
 
 function dealerCardValues() {
@@ -197,213 +183,17 @@ function gameLogic() {
     console.log("Hit or Stand?");
   } else if (playerTotal > 21) {
     console.log("Player Bust! Dealer wins!");
-  }else { (playerTotal === 21)
+  }else if (playerTotal === 21) {
     console.log("Blackjack! Player wins!");
+  }
+  if (dealerTotal >= 17) {
+    console.log("Dealer stands")
+  }else if (dealerTotal === 21) {
+    console.log("Black Jack! Dealer Wins")
+  }else { (dealerTotal > 21) 
+    console.log("Dealer Busts! Player Wins!")
 }
 }
-
-
-
-  // Create function to deal player cards into player hand
-// function dealPlayerCards () {
-//     for (let i; i < 2; i++){
-//         let card = shuffledDeck.pop()
-//         playerHand.push(card); 
-//     }
-// }
-
-
-
-// // Build a 'master' deck of 'card' objects used to create shuffled decks
-// const masterDeck = buildMasterDeck();
-// renderDeckInContainer(masterDeck, document.getElementById('master-deck-container'));
-
-// // /*----- cached element references -----*/
-// // const shuffledContainer = document.getElementById('shuffled-deck-container');
-// // const startGameBtn = document.getElementById('start-game');
-// // const standBtn = document.getElementById('stand');
-// // const hitBtn = document.getElementById('hit');
-
-// /*----- app's state (variables) -----*/
-// let shuffledDeck;
-
-// // cached element references //
-// let dealerHand = [];
-// let playerHand = [];
-// // let dealerScore = 0;
-// // let playerScore = 0;
-
-// /*----- event listeners -----*/
-// startGameBtn.addEventListener('click', startGame);
-// hitBtn.addEventListener('click', hit);
-// standBtn.addEventListener('click', stand);
-
-// /*----- functions -----*/
-
-// // Create function to deal player cards into player hand
-// function dealPlayerCards () {
-//     for (let i; i < 2; i++){
-//         let card = shuffledDeck.pop()
-//         playerHand.push(card); 
-//     }
-// }
-
-// function getNewShuffledDeck() {
-//     // Create a copy of the masterDeck (leave masterDeck untouched!)
-//     const tempDeck = [...masterDeck];
-//     const newShuffledDeck = [];
-//     while (tempDeck.length) {
-//       // Get a random index for a card still in the tempDeck
-//       const rndIdx = Math.floor(Math.random() * tempDeck.length);
-//       // Note the [0] after splice - this is because splice always returns an array and we just want the card object in that array
-//       newShuffledDeck.push(tempDeck.splice(rndIdx, 1)[0]);
-//     }
-//     return newShuffledDeck;
-//   }
-  
-//   function renderNewShuffledDeck() {
-//     // Create a copy of the masterDeck (leave masterDeck untouched!)
-//     shuffledDeck = getNewShuffledDeck();
-//     renderDeckInContainer(shuffledDeck, shuffledContainer);
-//   }
-  
-//   function renderDeckInContainer(deck, container) {
-//     container.innerHTML = '';
-//     // Let's build the cards as a string of HTML
-//     let cardsHtml = '';
-//     deck.forEach(function(card) {
-//       cardsHtml += `<div class="card ${card.face}"></div>`;
-//     });
-//     // Or, use reduce to 'reduce' the array into a single thing - in this case a string of HTML markup 
-//     // const cardsHtml = deck.reduce(function(html, card) {
-//     //   return html + `<div class="card ${card.face}"></div>`;
-//     // }, '');
-//     container.innerHTML = cardsHtml;
-//   }
-  
-//   function buildMasterDeck() {
-//     const deck = [];
-//     // Use nested forEach to generate card objects
-//     suits.forEach(function(suit) {
-//       ranks.forEach(function(rank) {
-//         deck.push({
-//           // The 'face' property maps to the library's CSS classes for cards
-//           face: `${suit}${rank}`,
-//           // Setting the 'value' property for game of blackjack, not war
-//           value: Number(rank) || (rank === 'A' ? 11 : 10)
-//         });
-//       });
-//     });
-//     return deck;
-//   }
-  
-
-//   // We call init, because we want to initialize our state when the page loads
-
-//   function init() {
-//     dealPlayerCards();
-//     getNewShuffledDeck() ;
-//     renderNewShuffledDeck() ;
-//     buildMasterDeck();
-//   }
-
-//   init();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//////////////////////////////Card Deck Starter Code below ////////////////////////////////////////////
-
-
-// /*----- functions -----*/
-// function getNewShuffledDeck() {
-//   // Create a copy of the masterDeck (leave masterDeck untouched!)
-//   const tempDeck = [...masterDeck];
-//   const newShuffledDeck = [];
-//   while (tempDeck.length) {
-//     // Get a random index for a card still in the tempDeck
-//     const rndIdx = Math.floor(Math.random() * tempDeck.length);
-//     // Note the [0] after splice - this is because splice always returns an array and we just want the card object in that array
-//     newShuffledDeck.push(tempDeck.splice(rndIdx, 1)[0]);
-//   }
-//   return newShuffledDeck;
-// }
-
-// // function renderNewShuffledDeck() {
-// //   // Create a copy of the masterDeck (leave masterDeck untouched!)
-// //   shuffledDeck = getNewShuffledDeck();
-// //   renderDeckInContainer(shuffledDeck, shuffledContainer);
-// // }
-
-// // function renderDeckInContainer(deck, container) {
-// //   container.innerHTML = '';
-// //   // Let's build the cards as a string of HTML
-// //   let cardsHtml = '';
-// //   deck.forEach(function(card) {
-// //     cardsHtml += `<div class="card ${card.face}"></div>`;
-// //   });
-// //   // Or, use reduce to 'reduce' the array into a single thing - in this case a string of HTML markup 
-// //   // const cardsHtml = deck.reduce(function(html, card) {
-// //   //   return html + `<div class="card ${card.face}"></div>`;
-// //   // }, '');
-// //   container.innerHTML = cardsHtml;
-// // }
-
-// function buildMasterDeck() {
-//   const deck = [];
-//   // Use nested forEach to generate card objects
-//   suits.forEach(function(suit) {
-//     ranks.forEach(function(rank) {
-//       deck.push({
-//         // The 'face' property maps to the library's CSS classes for cards
-//         face: `${suit}${rank}`,
-//         // Setting the 'value' property for game of blackjack, not war
-//         value: Number(rank) || (rank === 'A' ? 11 : 10)
-//       });
-//     });
-//   });
-//   return deck;
-// }
-
-// renderNewShuffledDeck();
-
 
 
 
@@ -432,20 +222,3 @@ function gameLogic() {
         //2.3.1 Dealer will stay, move to compare function
     //2.4.0. if dealer has 21
         //Show blackjack message and end turn, Show Dealer wins and game over, option to start new game  
-
-
-
-
- // Compare function
-    // Compare dealer and player card values which are the deck variables   
-        //define constants
-
-        //define variables
-
-        //define methods
-
-        //define classes
-
-        //define callbacks
-
-        //store elements on the page
